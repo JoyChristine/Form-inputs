c3c7dee4eac590951317beb28729e8e6787ef3b4
Joy Christine <joychristin2@gmail.com>
2022-03-09 23:58:21 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// $(document).ready(function() {
+//   
+//   var flavor = $("input:radio[name=flavor]:checked").val();
+// });
+var beverage = $("#beverage").val();
+var flavor = $("input:radio[name=flavor]:checked").val();
+var dob = $("#born").val();
+var favoriteColor = $("#color").val();
